151f150a857d6b8c0989a9608f4359a5d75d211f
= <=>
2013-12-08 14:18:03 +0100

    following other users is now working

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,28 @@
 class RelationshipsController < ApplicationController
+
+    def create
+
+        @relationshp = Relationship.new
+        @relationshp.followed_id = params[:followed_id]
+        @relationshp.follower_id = current_user.id
+
+        if @relationshp.save
+
+            redirect_to User.find params[:followed_id]
+        else
+
+            flash[:error] = "Couldn't Follow"
+            redirect_to root_url
+        end
+    end
+
+    def destroy
+
+        @relationship = Relationship.find(params[:id])
+
+        @followed = @relationship.followed_id
+        @relationship.destroy
+
+        redirect_to user_path @followed
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,14 @@ class User < ActiveRecord::Base
 
   has_many :ribbits
 
-  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id"
 
-  has_many :followed_relationships, classname: "Relationship", foreing_key: "Follower_id"
+  has_many :followed_relationships, class_name: "Relationship", foreign_key: "Follower_id"
 
   has_many :followers, through: :follower_relationships
   has_many :followeds, through: :followed_relationships
 
-  def followign? user
+  def following? user
 
     self.followeds.include? user
   end
